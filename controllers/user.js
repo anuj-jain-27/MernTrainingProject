f669f48102c5e680b49f0573754a9c323374d61a
anuj-jain-27 <januj2710@gmail.com>
2021-08-24 14:59:58 +0530

    first Integration backend and frontend

--- controllers/user.js
+++ controllers/user.js
@@ -16,6 +16,7 @@ exports.getUserById = (req,res,next,id)=>{
 }
 
 exports.getUser = (req,res) => {
+    // req.profile is populated using getUserById
     req.profile.salt = undefined;
     req.profile.encry_password = undefined;
     return res.json(req.profile);
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 
 const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
+const productRoutes = require("./routes/product")
+const orderRoutes = require("./routes/order")
 
 const app = express();
 
@@ -30,8 +33,13 @@ app.use(cors());
 // Routes
 app.use("/api",authRoutes)
 app.use("/api",userRoutes)
+app.use("/api",categoryRoutes)
+app.use("/api",productRoutes)
+app.use("/api",orderRoutes)
 
-app.listen(5000,()=>{
+
+
+app.listen(3000,()=>{
     console.log("Server started")
 })
 
